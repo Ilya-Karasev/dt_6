35f034a2019966a4abfb6c258ef531d06f7ac113
Ilya-Karasev <uvp112.karasev.i@mail.ru>
2023-10-22 15:34:29 +0300

    Main.java changed?... hey, first line is wrong

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 class Main {
     public static void main(String[] args) {
+        System.out.println("Это добавлено в ветке main");
         System.out.println("Это тоже добавлено в ветке dev");
         System.out.println("И это тоже добавлено в ветке dev. Интересно, правда?");
         System.out.println("Что? Хочешь разнообразия? Окей, как насчёт 2+2=4?");
+        System.out.println("Что? Первая строка пропала? Странно, не заметил. Ну ладно, сейчас верну");
     }
 } 
