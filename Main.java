3c3c66df5fedadcfde32f8b4e962d82d987733ac
Ilya-Karasev <uvp112.karasev.i@mail.ru>
2023-10-22 15:30:47 +0300

    Main.java changed?... again

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
         System.out.println("Это тоже добавлено в ветке dev");
+        System.out.println("И это тоже добавлено в ветке dev. Интересно, правда?");
     }
 } 
