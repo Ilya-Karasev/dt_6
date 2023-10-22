24975ebd4bc8d03c22b22b007732abecdddad70a
Ilya-Karasev <uvp112.karasev.i@mail.ru>
2023-10-22 15:41:59 +0300

    Main.java changed x2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main");
-        System.out.print("Это тоже добавлено в ветке main");
+        System.out.println("Это добавлено в ветке main");
+        System.out.println("Это тоже добавлено в ветке main");
+        System.out.println("Тебе и здесь нужно разнообразие? Слушай, скажи спасибо, что я хотя бы добавил отступы между сообщениями");
     }
 } 
